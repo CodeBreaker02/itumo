78b62d5ba6764a425d699cac848558fb31847beb
codebreaker02 <jordan.vitou@epitech.eu>
2024-05-23 22:28:30 +0100

    feat: add remove button on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import LoaderTextIcon from "@/components/ui/loader";
 import { Button } from "@/components/ui/button";
-import { AlertTitle, Alert, AlertDescription } from "@/components/ui/alert";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 const Home: React.FC = () => {
   const router = useRouter();
@@ -45,6 +45,11 @@ const Home: React.FC = () => {
     }
   };
 
+  const handleRemove = () => {
+    setVideoFile(null);
+    setPreviewUrl(null);
+  };
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     noClick: !!previewUrl,
@@ -53,14 +58,14 @@ const Home: React.FC = () => {
   });
 
   return (
-    <main className="flex flex-col items-center p-4 sm:p-10 md:p-24 min-h-screen mt-20">
-      <div className="text-center text-4xl sm:text-5xl md:text-7xl font-medium mb-5 sm:mb-10">
+    <main className="flex flex-col items-center p-4 sm:p-6 lg:p-8 min-h-screen mt-20">
+      <div className="text-center text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-medium mb-5 sm:mb-8 lg:mb-10">
         Echoes across cultures.
         <br />
         AI-Enhanced Multilingual Video Subtitles
       </div>
-      <div className="flex-col justify-center items-center w-full sm:w-3/4 lg:w-2/3">
-        <Alert className="bg-primary/60 text-white">
+      <div className="flex flex-col justify-center items-center w-full sm:w-3/4 lg:w-2/3">
+        <Alert className="bg-primary/60 text-white mb-4">
           <AlertTitle>Hey Pal !!!</AlertTitle>
           <AlertDescription>
             Use short video{" "}
@@ -70,7 +75,7 @@ const Home: React.FC = () => {
         </Alert>
         <div
           {...getRootProps()}
-          className="text-xl bg-primary/10 border-2 border-dashed border-muted-foreground rounded-lg p-4 sm:p-6 md:p-10 text-center w-full h-[500px] flex flex-col justify-center items-center overflow-hidden"
+          className="text-xl bg-primary/10 border-2 border-dashed border-muted-foreground rounded-lg p-4 sm:p-6 lg:p-10 text-center w-full h-[300px] sm:h-[400px] lg:h-[500px] flex flex-col justify-center items-center overflow-hidden"
         >
           <input {...getInputProps()} />
           {isUploading ? (
@@ -82,9 +87,14 @@ const Home: React.FC = () => {
                 className="max-h-full max-w-full rounded-lg shadow-md mb-4"
                 controls
               />
-              <Button size={"lg"} onClick={handleUpload}>
-                Upload Video
-              </Button>
+              <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+                <Button size={"lg"} onClick={handleUpload}>
+                  Upload Video
+                </Button>
+                <Button size={"lg"} variant="outline" onClick={handleRemove}>
+                  Remove Video
+                </Button>
+              </div>
             </>
           ) : isDragActive ? (
             <p>Drop the video here ...</p>
